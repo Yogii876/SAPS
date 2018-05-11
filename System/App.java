a31556d468e02b85a4bcc05cc0bd3161dd43bc9f
Yogii876 <yohann.brown@gmail.com>
2018-05-11 15:43:49 -0500

    Changes Made on the 11th v2

--- System/App.java
+++ System/App.java
@@ -1,35 +1,43 @@
 package System;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import Core.*;
 import CSV.*;
 
-import CSV.*;
-import Core.*;
 
 public class App {
 	private ArrayList<Student> students;
 	private ArrayList<CAPE> offeredSubjects = new ArrayList<CAPE>();
 	private Point point = new Point();
+	private Map<Integer, Map<String, Integer>> studentMapping = new HashMap<Integer, Map<String, Integer>>();
 	
 	public App () {
 		students = (new Reader("c:/users/yohan/Downloads/students.csv")).getStudents();
-		
-		for(int i = 0; i<offeredSubjects.size()-1; i++) {
-			for(int j =0; j<students.size()-1; j++) {
-				students.get(j).calcPoints(point, offeredSubjects.get(i));
-			}
 			
 		// creating n by m matrix, where n is the number of students and m the number of subjects offered.
 		int n = students.size();
 		int m = offeredSubjects.size();
 		
-		int[][] weight = new int[n][m];
+		
 		for (int k = 0; k < n-1; k++) {
-			for (int j = 0; j < m-1; m++) {
-				weight[k][j] = students.get(k).getMapping(j).getPoints();
+			for (int j = 0; j < m-1; j++) {
+				students.get(k).calcPoints(point, offeredSubjects.get(j));
+			studentMapping.put(students.get(k).getSid(), students.get(k).getMapping());
+			
+		
+				
+				
+				
+				
+				
+				
+				
+				
+				
 			}
-		}
+		
 		}
 		
 		
