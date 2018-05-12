42f8dcb83c11fada6e42f6fce5b6f23227921a5a
Yogii876 <yohann.brown@gmail.com>
2018-05-12 08:25:03 -0500

    Changes made on May 12th v2

--- Core/CAPE.java
+++ Core/CAPE.java
@@ -1,8 +1,13 @@
 package Core;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Comparator;
 
 public class CAPE extends Subject {
 	private String name, primary;
@@ -73,33 +78,28 @@ public class CAPE extends Subject {
 		return maxStudents;
 	}
 	
-	public LinkedHashMap<Integer, String> sortHashMapByValues(
-	        HashMap<Integer, String> passedMap) {
-	    List<Integer> mapKeys = new ArrayList<>(passedMap.keySet());
-	    List<String> mapValues = new ArrayList<>(passedMap.values());
-	    Collections.sort(mapValues);
-	    Collections.sort(mapKeys);
-
-	    LinkedHashMap<Integer, String> sortedMap =
-	        new LinkedHashMap<>();
-
-	    Iterator<String> valueIt = mapValues.iterator();
-	    while (valueIt.hasNext()) {
-	        String val = valueIt.next();
-	        Iterator<Integer> keyIt = mapKeys.iterator();
-
-	        while (keyIt.hasNext()) {
-	            Integer key = keyIt.next();
-	            String comp1 = passedMap.get(key);
-	            String comp2 = val;
-
-	            if (comp1.equals(comp2)) {
-	                keyIt.remove();
-	                sortedMap.put(key, val);
-	                break;
+	 public Map<Integer, Integer> sortStudents()
+	    {
+	
+	        List<Entry<Integer, Integer>> list = new LinkedList<Entry<Integer, Integer>>(eligibleStudents.entrySet());
+	
+	        // Sorting the list based on values
+	        Collections.sort(list, new Comparator<Entry<Integer, Integer>>()
+	        {
+	            public int compare(Entry<Integer, Integer> o1,
+	                    Entry<Integer, Integer> o2)
+	            {
+	                    return o2.getValue().compareTo(o1.getValue());
 	            }
+	        });
+	
+	        // Maintaining insertion order with the help of LinkedList
+	        Map<Integer, Integer> sortedMap = new LinkedHashMap<Integer, Integer>();
+	        for (Entry<Integer, Integer> entry : list)
+	        {
+	            sortedMap.put(entry.getKey(), entry.getValue());
 	        }
+	
+	        return sortedMap;
 	    }
-	    return sortedMap;
-	}
 }
