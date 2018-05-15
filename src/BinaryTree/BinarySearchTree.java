ae4768315e11e9476c732c0b87a560bf64fa720d
Yogii876 <yohann.brown@gmail.com>
2018-05-14 20:01:08 -0500

    Added a binary tree

--- src/BinaryTree/BinarySearchTree.java
+++ src/BinaryTree/BinarySearchTree.java
@@ -1,37 +1,10 @@
 package BinaryTree;
 import Core.*;
 
-/******************************************************************************
- *  Compilation:  javac BST.java
- *  Execution:    java BST
- *  Dependencies: StdIn.java StdOut.java Queue.java
- *  Data files:   https://algs4.cs.princeton.edu/32bst/tinyST.txt  
- *
- *  A symbol table implemented with a binary search tree.
- * 
- *  % more tinyST.txt
- *  S E A R C H E X A M P L E
- *  
- *  % java BST < tinyST.txt
- *  A 8
- *  C 4
- *  E 12
- *  H 5
- *  L 11
- *  M 9
- *  P 10
- *  R 3
- *  S 0
- *  X 7
- *
- ******************************************************************************/
-
-import java.util.NoSuchElementException;
-
 /**
  *  The {@code BST} class represents an ordered symbol table of generic
  *  key-value pairs.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ *  It supports the usual <em>insert</em>, <em>get</em>, <em>contains</em>,
  *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
  *  It also provides ordered methods for finding the <em>minimum</em>,
  *  <em>maximum</em>, <em>floor</em>, <em>select</em>, <em>ceiling</em>.
@@ -48,7 +21,7 @@ import java.util.NoSuchElementException;
  *  the key type implements the {@code Comparable} interface and calls the
  *  {@code compareTo()} and method to compare two keys. It does not call either
  *  {@code equals()} or {@code hashCode()}.
- *  The <em>put</em>, <em>contains</em>, <em>remove</em>, <em>minimum</em>,
+ *  The <em>insert</em>, <em>contains</em>, <em>remove</em>, <em>minimum</em>,
  *  <em>maximum</em>, <em>ceiling</em>, <em>floor</em>, <em>select</em>, and
  *  <em>rank</em>  operations each take
  *  linear time in the worst case, if the tree becomes unbalanced.
@@ -77,10 +50,6 @@ public class BinarySearchTree {
             this.key = key;
             this.val = val;
             this.size = size;
-        }
-        
-        public String getKey() {
-        	return this.key;
         }
     }
 
@@ -156,87 +125,21 @@ public class BinarySearchTree {
      * @param  val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(String key, Student val) {
-        if (key == null) throw new IllegalArgumentException("calls put() with a null key");
-        root = put(root, key, val);
-        assert check();
+    public void insert(String key, Student val) {
+        if (key == null) throw new IllegalArgumentException("calls insert() with a null key");
+        root = insert(root, key, val);
     }
 
-    private Node put(Node x, String key, Student val) {
+    private Node insert(Node x, String key, Student val) {
         if (x == null) return new Node(key, val, 1);
         int cmp = key.compareTo(x.key);
-        if      (cmp < 0) x.left  = put(x.left,  key, val);
-        else if (cmp > 0) x.right = put(x.right, key, val);
+        if      (cmp < 0) x.left  = insert(x.left,  key, val);
+        else if (cmp > 0) x.right = insert(x.right, key, val);
         else              x.val   = val;
         x.size = 1 + size(x.left) + size(x.right);
         return x;
     }
   
 
-    
-  /*************************************************************************
-    *  Check integrity of BST data structure.
-    ***************************************************************************/
-    /**
-    private boolean check() {
-        if (!isBST())            StdOut.println("Not in symmetric order");
-        if (!isSizeConsistent()) StdOut.println("Subtree counts not consistent");
-        if (!isRankConsistent()) StdOut.println("Ranks not consistent");
-        return isBST() && isSizeConsistent() && isRankConsistent();
-    }
-
-    // does this binary tree satisfy symmetric order?
-    // Note: this test also ensures that data structure is a binary tree since order is strict
-    private boolean isBST() {
-        return isBST(root, null, null);
-    }
-
-    // is the tree rooted at x a BST with all keys strictly between min and max
-    // (if min or max is null, treat as empty constraint)
-    // Credit: Bob Dondero's elegant solution
-    private boolean isBST(Node x, String min, String max) {
-        if (x == null) return true;
-        if (min != null && x.key.compareTo(min) <= 0) return false;
-        if (max != null && x.key.compareTo(max) >= 0) return false;
-        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
-    } 
-
-    // are the size fields correct?
-    private boolean isSizeConsistent() { return isSizeConsistent(root); }
-    private boolean isSizeConsistent(Node x) {
-        if (x == null) return true;
-        if (x.size != size(x.left) + size(x.right) + 1) return false;
-        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
-    } 
-
-    // check that ranks are consistent
-    private boolean isRankConsistent() {
-        for (int i = 0; i < size(); i++)
-            if (i != rank(select(i))) return false;
-        for (String key : keys())
-            if (key.compareTo(select(rank(key))) != 0) return false;
-        return true;
-    }**/
-
-
-    /**
-     * Unit tests the {@code BST} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) { 
-        BST<String, Integer> st = new BST<String, Integer>();
-        for (int i = 0; !StdIn.isEmpty(); i++) {
-            String key = StdIn.readString();
-            st.put(key, i);
-        }
-
-        for (String s : st.levelOrder())
-            StdOut.println(s + " " + st.get(s));
-
-        StdOut.println();
-
-        for (String s : st.keys())
-            StdOut.println(s + " " + st.get(s));
-    }
+   
 }
