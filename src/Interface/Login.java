9ce3777815a62f94c703df462700ca1b60d31219
Yogii876 <yohann.brown@gmail.com>
2018-05-22 00:59:20 -0500

    Final v3

--- src/Interface/Login.java
+++ src/Interface/Login.java
@@ -37,16 +37,12 @@ public class Login {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login window = new Login();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			Login window = new Login();
+			window.frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
